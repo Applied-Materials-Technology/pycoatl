94be1e732742f92c786435a53bb0466ecb26e384
Rory Spencer <rory.spencer@ukaea.uk>
2024-01-02 15:25:32 +0000

    Moose test update

--- tests/test_importmoose.py
+++ tests/test_importmoose.py
@@ -4,9 +4,11 @@
 
 import pytest
 from pycoatl.spatialdata.importmoose import moose_to_spatialdata
+import pyvista as pv
 
 def test_import():
     efile = 'data/moose_output.e'
     sd = moose_to_spatialdata(efile)
-    sd.data_sets[-1].plot(scalars='disp_y',cpos='xy')
+    pv.global_theme.colorbar_orientation = 'vertical'
+    sd.data_sets[-1].plot(scalars='mechanical_strain_yy',cpos='xy',)
     # Should plot the data.
