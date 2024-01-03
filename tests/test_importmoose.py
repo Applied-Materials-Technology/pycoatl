ae39ef9ba838f74fd5b28dd507f037cca8a3bdb9
Rory Spencer <rory.spencer@ukaea.uk>
2024-01-03 15:11:09 +0000

    Moved spatialdata functions from wrapper to class

--- tests/test_importmoose.py
+++ tests/test_importmoose.py
@@ -5,6 +5,7 @@
 import pytest
 from pycoatl.spatialdata.importmoose import moose_to_spatialdata
 import pyvista as pv
+from pycoatl.spatialdata.spatialdatawrapper import get_standard_theme
 
 #def test_import():
 #    efile = 'data/moose_output.e'
@@ -20,3 +21,10 @@ def test_new_import():
     pv.global_theme.colorbar_orientation = 'vertical'
     sd.data_sets[-1].plot(scalars='mechanical_strain_yy',cpos='xy',)
     # Should plot the data.
+
+def test_differentiation():
+    efile = 'data/moose_output.e'
+    sd = moose_to_spatialdata(efile)
+    sd.window_differentation()
+    assert 'exx' in sd.data_sets[-1].array_names
+    sd.data_sets[-1].plot(scalars='eyy',theme=get_standard_theme())
