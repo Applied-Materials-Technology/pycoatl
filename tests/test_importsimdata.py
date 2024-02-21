bb57322deb3eb5f7bd92705f03c0e6a5ab9fb64a
Rory Spencer <rory.spencer@ukaea.uk>
2024-02-21 09:25:47 +0000

    With tests

---
+++ tests/test_importsimdata.py
@@ -0,0 +1,19 @@
+#
+#
+
+from pycoatl.spatialdata.importsimdata import simdata_to_spatialdata
+from pycoatl.spatialdata.spatialdatawrapper import get_standard_theme
+from pathlib import Path
+from mooseherder import ExodusReader
+import pytest
+
+
+def test_import():
+    #output_file = Path('/home/rspencer/pycoatl/data/moose-sim-1_out.e')
+    output_file = Path('/home/rspencer/moose_work/Viscoplastic_Creep/HVPF_Sat/Run/moose-workdir-1/moose-sim-152_out.e')
+    exodus_reader = ExodusReader(output_file)
+    all_sim_data = exodus_reader.read_all_sim_data()
+    dd = simdata_to_spatialdata(all_sim_data)
+    #assert dd._load[0] == pytest.approx(197.614)
+    #assert dd._load[-1] == pytest.approx(1086.025)
+    dd.data_sets[-1].plot(scalars='plastic_strain_yy',theme=get_standard_theme())
