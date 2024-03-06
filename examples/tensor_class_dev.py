70bd4f3e65cae81d0e9a9472e9081d9dec8dfe0b
Rory Spencer <rory.spencer@ukaea.uk>
2024-03-06 15:46:04 +0000

    Further tensor developments

--- examples/tensor_class_dev.py
+++ examples/tensor_class_dev.py
@@ -25,11 +25,11 @@ all_sim_data = exodus_reader.read_all_sim_data()
 
 
 # %%
-
+# maybe also have scalar field
 class tensor_field_base(ABC):
 
     @abstractmethod
-    def rotate(self,rot_mat: npt.NDArray) -> None:
+    def rotate(self,rotation_matrix: npt.NDArray) -> None:
         pass
 
     @abstractmethod
@@ -47,12 +47,44 @@ class vector_field(tensor_field_base):
     def __init__(self,input_data: npt.NDArray):
 
         self.data = input_data
+        self.n_points = input_data.shape[0]
+        self.n_steps = input_data.shape[2]
 
-    def rotate(self, rot_mat: npt.NDArray) -> None:
-        return super().rotate(rot_mat)
+    def rotate(self, rotation_matrix: npt.NDArray) -> None:
+        #rot_mat = np.array([[0,1,0],[-1,0,0],[0,0,1]])
+        if rotation_matrix.shape not in [(3,3),(4,4)]:
+            raise RuntimeError('Rotation matrix is {}. Should be (3,3) or a (4,4) vtk Transformation matrix.'.format(rotation_matrix.shape))
+
+        if rotation_matrix.shape == (4,4): # Given vtk / pyvista transformation matrix
+            rotation_matrix = rotation_matrix[0:3,0:3]
+        
+
+        rot_field_0 = np.swapaxes(np.tile(rotation_matrix[0,:],(self.n_points,self.n_steps,1)),1,2)
+        rot_field_1 = np.swapaxes(np.tile(rotation_matrix[1,:],(self.n_points,self.n_steps,1)),1,2)
+        rot_field_2 = np.swapaxes(np.tile(rotation_matrix[2,:],(self.n_points,self.n_steps,1)),1,2)
+
+        x_comp = vector_field.dot_product_field(self.data,rot_field_0)
+        y_comp = vector_field.dot_product_field(self.data,rot_field_1)
+        z_comp = vector_field.dot_product_field(self.data,rot_field_2)
+
+        rotated_field = np.stack((x_comp,y_comp,z_comp),axis=1)
+        self.data = rotated_field
     
     def get_component_field(self, component: int, time_step: int) -> npt.NDArray:
         return self.data[:,component,time_step]
+    
+    @staticmethod
+    def dot_product_field(a: npt.NDArray,b: npt.NDArray) -> npt.NDArray: 
+        """Dot product between two vector fields
+
+        Args:
+            a (npt.NDArray): Vector 1
+            b (npt.NDArray): Vector 2
+
+        Returns:
+            npt.NDArray: Scalar output
+        """
+        return np.sum(a*b,axis=1)
     
     
 class rank_two_field(tensor_field_base):
@@ -63,7 +95,7 @@ class rank_two_field(tensor_field_base):
 
         self.data = input_data
 
-    def rotate(self, rot_mat: npt.NDArray) -> None:
+    def rotate(self, rotation_matrix: npt.NDArray) -> None:
         return super().rotate(rot_mat)
     
     def get_component_field(self, component: Sequence , time_step: int) -> npt.NDArray:
@@ -97,7 +129,7 @@ class tensor_field():
         self.n_points = self.data.shape[0]
         self.n_steps = self.data.shape[2]
 
-    def rotate(self,rot_mat: npt.NDArray):
+    def rotate(self,rotation_matrix: npt.NDArray):
         pass
 
     def trace(self):
@@ -129,8 +161,9 @@ print(input_data.shape)
 # %%
 displacements = vector_field(input_data)
 print(displacements.rank)
-print(displacements.get_component_field(0,1))
-
+#print(displacements.get_component_field(0,1))
+test = vector_field.dot_product_field(displacements.data,displacements.data)
+print(test.shape)
 #%%
 dummy = np.zeros_like(all_sim_data.elem_vars[('strain_xx',1)])
 strain_data = np.dstack((all_sim_data.elem_vars[('strain_xx',1)],dummy,all_sim_data.elem_vars[('strain_yy',1)],dummy,dummy,dummy,dummy,dummy,all_sim_data.elem_vars[('strain_zz',1)]))
@@ -148,4 +181,27 @@ dummy = tensor_field(test_input,'dummy')
 print(dummy.rank)
 rot_mat = np.array([[0.996194,0.087155,0],[-0.08715,0.996194,0],[0,0,1]])
 print(rot_mat)
+# %%
+p=150
+plt.plot([0,input_data[p,0,1]],[0,input_data[p,1,1]])
+print(input_data[p,:,1])
+#%%
+rot_mat = np.array([[0,1,0],[-1,0,0],[0,0,1]])
+rot_field_0 = np.swapaxes(np.tile(rot_mat[0,:],(displacements.n_points,displacements.n_steps,1)),1,2)
+rot_field_1 = np.swapaxes(np.tile(rot_mat[1,:],(displacements.n_points,displacements.n_steps,1)),1,2)
+rot_field_2 = np.swapaxes(np.tile(rot_mat[2,:],(displacements.n_points,displacements.n_steps,1)),1,2)
+
+x_comp = vector_field.dot_product_field(displacements.data,rot_field_0)
+y_comp = vector_field.dot_product_field(displacements.data,rot_field_1)
+z_comp = vector_field.dot_product_field(displacements.data,rot_field_2)
+
+rot_vec = np.stack((x_comp,y_comp,z_comp),axis=1)
+print(rot_vec.shape)
+# %%
+rot_mat_t = np.array([[0,1,0,0],[-1,0,0,0],[0,0,1,0],[0,0,0,1]])
+# %%
+if rot_mat_t.shape==(4,4):
+    print(rot_mat_t[0:3,0:3])
+# %%
+displacements.rotate(rot_mat)
 # %%
