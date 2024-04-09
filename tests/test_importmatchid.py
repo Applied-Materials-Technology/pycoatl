732c2b095c6dc3e952fc0b18ac0b676a4cb7acd0
Rory Spencer <rory.spencer@ukaea.uk>
2024-04-09 12:57:47 +0100

    Corrected matchid z coords

--- tests/test_importmatchid.py
+++ tests/test_importmatchid.py
@@ -10,8 +10,8 @@ def test_import_2023():
     folderpath = Path('data/matchid_dat_2023')
     loadfile = Path('data/matchid_load.csv')
     sd = matchid_to_spatialdata(folderpath,loadfile,version='2023',loadfile_format='Davis')
-    sd.plot(data_field='displacements',component=[1],time_step=-1)
-    sd.plot(data_field='strains',component=[1,1],time_step=-1)
+    sd.plot(data_field='displacement',component=[2],time_step=-1)
+    sd.plot(data_field='total_strain',component=[1,1],time_step=-1)
     # Should plot the data.
     assert sd.load[-1] == pytest.approx(405.577)
 
@@ -19,5 +19,5 @@ def test_import_2024():
     folderpath = Path('data/matchid_dat_2024')
     loadfile = Path('data/Image.csv')
     sd = matchid_to_spatialdata(folderpath,loadfile,version='2024.1',loadfile_format='Image.csv')
-    sd.plot(data_field='displacements',component=[1],time_step=-1)
+    sd.plot(data_field='displacement',component=[1],time_step=-1)
     
